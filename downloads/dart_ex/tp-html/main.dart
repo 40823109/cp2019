8808e800ea5ef9f89b1f3b491dced20b7b766fd9
40823109 <40823109@gm.nfu.edu.tw>
2019-12-05 11:09:39 +0800

    tp-html

--- downloads/dart_ex/tp-html/main.dart
+++ downloads/dart_ex/tp-html/main.dart
@@ -1,127 +1,27 @@
-/*
-void main() {
- print("Hello, World!");
-}
-*/
-
-/*
- * 主函式也可以利用 => 定義單行的函式內容
-main() => print("Hello, World!");
-*/
-
-/*
-import 'dart:math' as math;
-
-void main() {
-  var n = 0; // °F
-  print("The °F increases as:\n");
-  for (int temperatures = 0; temperatures <= 10; temperatures++) {
-    n =(temperatures*1.8+32).round().toInt();
-    print("Count $temperatures temperatures:\t $n temperatures");
-  }
-}
-*/
-
-/*
-import 'dart:math' as math;
-
-int temperatures = 0;
-const int NO_temperatures = 10;
-
-void main() {
-  print("The °F increases as:\n");
-  for (int temperatures = 0) {
-    temperaturesCount = calculatetemperatures(temperatures);
-    print("Count $temperatures temperatures:\t $temperaturesCount temperatures");
-  }
+CtoF(num c){
+return c*9/5 + 32;
 }
 
-int calculatetemperatures(int temperatures) {
-  return(temperatures*1.8+32)
-      .round()
-      .toInt();
+FtoC(num f){
+return (f - 32)*5/9;
 }
-*/
-
-import "dart:html";
-
-
-void main() {
-  querySelector("#submit").onClick.listen((e) => calctemperatures());
-}
-
-calctemperatures() {
-  // binding variables to html elements:
-  InputElement temperaturesInput = querySelector("#temperatures");
-  LabelElement output = querySelector("#output");
-  // getting input
-  String temperaturesString = temperaturesInput.value;
-  int temperatures = int.parse(temperaturesString);
-  // calculating and setting output:
-  output.innerHtml = "${calculatetemperatures(temperatures)}";
-}
-
-int calculatetemperatures(int temperatures) {
-  return (temperatures*1.8+32)
-      .round()
-      .toInt();
-}
-
-/*
-html
-
-temperatures<input type="number" id="temperatures" value="0" min="0" max="100"> 
-°C
-<input type="button" id="submit" value="Calculate"/>
-<br/>°F: <label id="output"></label>
-
-css
-
-body {
-  color: white;
-  font-size: 50px;
-}
-
-input, select, textarea {
-font-size: 100%;
-}
-*/
-/*
-import 'dart:html';
-
-InputElement task;
-UListElement list;
 
 main() {
-  task = querySelector('#task');
-  list = querySelector('#list');
-  task.onChange.listen((e) => addItem());
-}
-
-void addItem() {
-  var newTask = LIElement();
-  newTask.text = task.value;
-  task.value = '';
-  list.children.add(newTask);
-}
-*/
-
-/*
-html
-  
-  <input id="task" type="text" placeholder=
-  "What do you want to do?"/>
-  <ul id="list"/>
-  
-css
-
-body {
-  color: white;
-  font-size: 50px;
-}
-
-input, select, textarea {
-font-size: 100%;
-}
-*/
-
+  int len;
+  var type;
+  var number;
+  List temp = ["20C", "30C", "50F", "40C", "23F"];
+  for (var data in temp) {
+    len = data.length;
+    type = data[len-1];
+    number = data.substring(0, len-1);
+    number = int.parse(number);
+    
+    if (type == "C"){
+       print("攝氏 $number 度 = 華氏 ${CtoF(number)} 度");
+    }else{
+      print("華氏 $number 度 = 攝氏 ${FtoC(number)} 度");
+    }
+      
+  } 
+} 
